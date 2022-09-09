48bfa07fa799db3214fd807511752f31fd80ac42
Pete Pickerill <pete@liquibase.com>
2022-09-09 13:22:36 -0500

    Testing WebApp Update

--- src/html/SamplePage.php
+++ src/html/SamplePage.php
@@ -2,7 +2,7 @@
 <?php include "../inc/dbinfo.inc"; ?>
 <html>
 <body>
-<h1>Sample page</h1>
+<h1>Sample Page - Brought To You By AWS CodePipeline & CodeDeploy</h1>
 <?php
 
   /* Connect to MySQL and select the database. */
