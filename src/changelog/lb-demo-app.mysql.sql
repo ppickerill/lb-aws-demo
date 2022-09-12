eda75e01a61d76a9fdd251d8fca957447591bda9
Pete Pickerill <pete@liquibase.com>
2022-09-12 08:23:21 -0500

    Complete changelog with EMPLOYEES def

--- src/changelog/lb-demo-app.mysql.sql
+++ src/changelog/lb-demo-app.mysql.sql
@@ -1,3 +1,11 @@
 -- liquibase formatted changelog
--- changeset ppickerill:add-phone-number
+-- changeset ppickerill:create-employees-table
+CREATE TABLE `EMPLOYEES` (
+  `ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(45) DEFAULT NULL,
+  `ADDRESS` varchar(90) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+);
+
+-- changeset ppickerill:add-phone-number-to-employees
 ALTER TABLE sample.EMPLOYEES ADD PHONE_NUMBER varchar(100) NULL;
