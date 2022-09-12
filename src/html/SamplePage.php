22efbfe61e57aff87410c88a3af2264607c546b3
Pete Pickerill <pete@liquibase.com>
2022-09-12 12:12:58 -0500

    Visual tweaks

--- src/html/SamplePage.php
+++ src/html/SamplePage.php
@@ -1,27 +1,28 @@
-
 <?php include "../inc/dbinfo.inc"; ?>
 <html>
+
 <head>
-<title>Liquibase Employee Database</title>
+  <title>Liquibase Employee Database</title>
   <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 </head>
+
 <body>
-<div id="centerColumn">
-<div id="header">
-    <h1>Liquibasers of the World</h1>
-    <h2>Unite & Take Over</h2>
-  </div>
-  <!--//end #headern//-->
-  <div id="nav">
-    <ul>
-      <li><a href-"#">Home</a></li>
-      <li><a href-"#">About</a></li>
-      <li><a href-"#">Gallery</a></li>
-      <li><a href-"#">Contact</a></li>
-    </ul>
-  </div>
-  <h1>Brought To You By AWS CodePipeline & Liquibase</h1>
-  <?php
+  <div id="centerColumn">
+    <div id="header">
+      <h1>Liquibasers of the World</h1>
+      <h2>Unite & Take Over</h2>
+    </div>
+    <!--//end #headern//-->
+    <div id="nav">
+      <ul>
+        <li><a href-"#">Home</a></li>
+        <li><a href-"#">About</a></li>
+        <li><a href-"#">Gallery</a></li>
+        <li><a href-"#">Contact</a></li>
+      </ul>
+    </div>
+    <h1>Brought To You By AWS CodePipeline & Liquibase</h1>
+    <?php
 
     /* Connect to MySQL and select the database. */
     $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
@@ -40,104 +41,109 @@
     if (strlen($employee_name) || strlen($employee_address)) {
       AddEmployee($connection, $employee_name, $employee_address);
     }
-  ?>
+    ?>
 
-  <!-- Input form -->
-  <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
-    <table border="0">
+    <!-- Input form -->
+    <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
+      <table border="0">
+        <tr>
+          <td>NAME</td>
+          <td>ADDRESS</td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" name="NAME" maxlength="45" size="30" />
+          </td>
+          <td>
+            <input type="text" name="ADDRESS" maxlength="90" size="60" />
+          </td>
+          <td>
+            <input type="submit" value="Add Data" />
+          </td>
+        </tr>
+      </table>
+    </form>
+
+    <!-- Display table data. -->
+    <table border="1" cellpadding="2" cellspacing="2">
       <tr>
+        <td>ID</td>
         <td>NAME</td>
         <td>ADDRESS</td>
       </tr>
-      <tr>
-        <td>
-          <input type="text" name="NAME" maxlength="45" size="30" />
-        </td>
-        <td>
-          <input type="text" name="ADDRESS" maxlength="90" size="60" />
-        </td>
-        <td>
-          <input type="submit" value="Add Data" />
-        </td>
-      </tr>
+
+      <?php
+
+      $result = mysqli_query($connection, "SELECT * FROM EMPLOYEES");
+
+      while ($query_data = mysqli_fetch_row($result)) {
+        echo "<tr>";
+        echo "<td>", $query_data[0], "</td>",
+        "<td>", $query_data[1], "</td>",
+        "<td>", $query_data[2], "</td>";
+        echo "</tr>";
+      }
+      ?>
+
     </table>
-  </form>
 
-  <!-- Display table data. -->
-  <table border="1" cellpadding="2" cellspacing="2">
-    <tr>
-      <td>ID</td>
-      <td>NAME</td>
-      <td>ADDRESS</td>
-    </tr>
-
-  <?php
-
-  $result = mysqli_query($connection, "SELECT * FROM EMPLOYEES");
-
-  while($query_data = mysqli_fetch_row($result)) {
-    echo "<tr>";
-    echo "<td>",$query_data[0], "</td>",
-        "<td>",$query_data[1], "</td>",
-        "<td>",$query_data[2], "</td>";
-    echo "</tr>";
-  }
-  ?>
-
-  </table>
-
-  <!-- Clean up. -->
-  <?php
+    <!-- Clean up. -->
+    <?php
 
     mysqli_free_result($result);
     mysqli_close($connection);
 
-  ?>
+    ?>
+    <div id="footer">
+      <h2> Brought to you by AWS CodePipeline & Liquibase </h2>
+    </div>
+
   </div>
-  <div id="footer">
-    <h2> Brought to you by AWS CodePipeline & Liquibase </h2>
-</div>
 </body>
+
 </html>
 
 
 <?php
 
 /* Add an employee to the table. */
-function AddEmployee($connection, $name, $address) {
-   $n = mysqli_real_escape_string($connection, $name);
-   $a = mysqli_real_escape_string($connection, $address);
+function AddEmployee($connection, $name, $address)
+{
+  $n = mysqli_real_escape_string($connection, $name);
+  $a = mysqli_real_escape_string($connection, $address);
 
-   $query = "INSERT INTO EMPLOYEES (NAME, ADDRESS) VALUES ('$n', '$a');";
+  $query = "INSERT INTO EMPLOYEES (NAME, ADDRESS) VALUES ('$n', '$a');";
 
-   if(!mysqli_query($connection, $query)) echo("<p>Error adding employee data.</p>");
+  if (!mysqli_query($connection, $query)) echo ("<p>Error adding employee data.</p>");
 }
 
 /* Check whether the table exists and, if not, create it. */
-function VerifyEmployeesTable($connection, $dbName) {
-  if(!TableExists("EMPLOYEES", $connection, $dbName))
-  {
-     $query = "CREATE TABLE EMPLOYEES (
+function VerifyEmployeesTable($connection, $dbName)
+{
+  if (!TableExists("EMPLOYEES", $connection, $dbName)) {
+    $query = "CREATE TABLE EMPLOYEES (
          ID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
          NAME VARCHAR(45),
          ADDRESS VARCHAR(90)
        )";
 
-     if(!mysqli_query($connection, $query)) echo("<p>Error creating table.</p>");
+    if (!mysqli_query($connection, $query)) echo ("<p>Error creating table.</p>");
   }
 }
 
 /* Check for the existence of a table. */
-function TableExists($tableName, $connection, $dbName) {
+function TableExists($tableName, $connection, $dbName)
+{
   $t = mysqli_real_escape_string($connection, $tableName);
   $d = mysqli_real_escape_string($connection, $dbName);
 
-  $checktable = mysqli_query($connection,
-      "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
+  $checktable = mysqli_query(
+    $connection,
+    "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'"
+  );
 
-  if(mysqli_num_rows($checktable) > 0) return true;
+  if (mysqli_num_rows($checktable) > 0) return true;
 
   return false;
 }
-?>                        
-                
+?>
