4f6643beb5fa2dd73138aad6b426c73dd03ca69f
Pete Pickerill <pete@liquibase.com>
2022-09-12 10:09:37 -0500

    Tweaking more

--- src/html/SamplePage.php
+++ src/html/SamplePage.php
@@ -2,104 +2,85 @@
 <?php include "../inc/dbinfo.inc"; ?>
 <html>
 <head>
-  <title>Fixed box layout</title>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Liquibase Employee Database</title>
   <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 </head>
 <body>
-<h1>Sample Page - Brought To You By AWS CodePipeline & Liquibase</h1>
-<?php
-
-  /* Connect to MySQL and select the database. */
-  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
-
-  if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
-
-  $database = mysqli_select_db($connection, DB_DATABASE);
-
-  /* Ensure that the EMPLOYEES table exists. */
-  VerifyEmployeesTable($connection, DB_DATABASE);
-
-  /* If input fields are populated, add a row to the EMPLOYEES table. */
-  $employee_name = htmlentities($_POST['NAME']);
-  $employee_address = htmlentities($_POST['ADDRESS']);
-
-  if (strlen($employee_name) || strlen($employee_address)) {
-    AddEmployee($connection, $employee_name, $employee_address);
-  }
-?>
-
 <div id="centerColumn">
-  <div id="header">
-    <h1>site title</h1>
-    <h2>fixed box layout</h2>
-  </div>
-  <!--//end #headern//-->
-  <div id="nav">
-    <ul>
-      <li><a href-"#">Home</a></li>
-      <li><a href-"#">About</a></li>
-      <li><a href-"#">Gallery</a></li>
-      <li><a href-"#">Contact</a></li>
-    </ul>
-  </div>
-  <!--//end #nav//-->
-  <h2>centerColumn</h2>
+  <h1>Sample Page - Brought To You By AWS CodePipeline & Liquibase</h1>
+  <?php
+
+    /* Connect to MySQL and select the database. */
+    $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+    if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+    $database = mysqli_select_db($connection, DB_DATABASE);
+
+    /* Ensure that the EMPLOYEES table exists. */
+    VerifyEmployeesTable($connection, DB_DATABASE);
+
+    /* If input fields are populated, add a row to the EMPLOYEES table. */
+    $employee_name = htmlentities($_POST['NAME']);
+    $employee_address = htmlentities($_POST['ADDRESS']);
+
+    if (strlen($employee_name) || strlen($employee_address)) {
+      AddEmployee($connection, $employee_name, $employee_address);
+    }
+  ?>
+
   <!-- Input form -->
-<form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
-  <table border="0">
+  <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
+    <table border="0">
+      <tr>
+        <td>NAME</td>
+        <td>ADDRESS</td>
+      </tr>
+      <tr>
+        <td>
+          <input type="text" name="NAME" maxlength="45" size="30" />
+        </td>
+        <td>
+          <input type="text" name="ADDRESS" maxlength="90" size="60" />
+        </td>
+        <td>
+          <input type="submit" value="Add Data" />
+        </td>
+      </tr>
+    </table>
+  </form>
+
+  <!-- Display table data. -->
+  <table border="1" cellpadding="2" cellspacing="2">
     <tr>
+      <td>ID</td>
       <td>NAME</td>
       <td>ADDRESS</td>
     </tr>
-    <tr>
-      <td>
-        <input type="text" name="NAME" maxlength="45" size="30" />
-      </td>
-      <td>
-        <input type="text" name="ADDRESS" maxlength="90" size="60" />
-      </td>
-      <td>
-        <input type="submit" value="Add Data" />
-      </td>
-    </tr>
+
+  <?php
+
+  $result = mysqli_query($connection, "SELECT * FROM EMPLOYEES");
+
+  while($query_data = mysqli_fetch_row($result)) {
+    echo "<tr>";
+    echo "<td>",$query_data[0], "</td>",
+        "<td>",$query_data[1], "</td>",
+        "<td>",$query_data[2], "</td>";
+    echo "</tr>";
+  }
+  ?>
+
   </table>
-</form>
 
-<!-- Display table data. -->
-<table border="1" cellpadding="2" cellspacing="2">
-  <tr>
-    <td>ID</td>
-    <td>NAME</td>
-    <td>ADDRESS</td>
-  </tr>
+  <!-- Clean up. -->
+  <?php
 
-<?php
+    mysqli_free_result($result);
+    mysqli_close($connection);
 
-$result = mysqli_query($connection, "SELECT * FROM EMPLOYEES");
-
-while($query_data = mysqli_fetch_row($result)) {
-  echo "<tr>";
-  echo "<td>",$query_data[0], "</td>",
-       "<td>",$query_data[1], "</td>",
-       "<td>",$query_data[2], "</td>";
-  echo "</tr>";
-}
-?>
-
-</table>
-
-<!-- Clean up. -->
-<?php
-
-  mysqli_free_result($result);
-  mysqli_close($connection);
-
-?>
-
-  <div id="footer"> <a target="_blank" href="http://validator.w3.org/" title="W3C HTML Validation">XHTML</a> | <a target="_blank" href="http://jigsaw.w3.org/css-validator/validator-uri.html" title="W3C CSS Validation">CSS</a> | <a target="_blank" href="http://www.w3.org/TR/WCAG10/" title="Web Content Accessibility Guidelines">WCAG</a> | <a target="_blank" rel="nofollow" href="http://www.csstinderbox.com">The CSS Tinderbox</a> </div>
-  <!--//end #footer//-->
-</div>
+  ?>
+  </div>
 </body>
 </html>
 
